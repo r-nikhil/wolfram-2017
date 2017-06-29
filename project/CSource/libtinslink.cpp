c8b387203c11fddcba48cc9129f94b80926d4432
Ian Johnson <ijohnson@wolfram.com>
2017-06-28 21:29:28 -0400

    Updating structure again to reflect new object based access

--- project/CSource/libtinslink.cpp
+++ project/CSource/libtinslink.cpp
@@ -4,6 +4,8 @@
 //for librarylink
 #include "WolframLibrary.h"
 
+#include <map>
+
 
 DLLEXPORT mint WolframLibrary_getVersion(){
   return WolframLibraryVersion;
@@ -22,45 +24,78 @@ libData) {
 using namespace Tins;
 
 
+std::map<int,Packet * > hash_table;
 
 
-int sniffeth_internal(int ms)
-{
-	SnifferConfiguration config;
-	config.set_filter("port 80");
-	config.set_promisc_mode(true);
-	config.set_snap_len(400);
+static int currentPacketID = 0;
 
-	Sniffer snifferObject("en0",config);
-
-	snifferObject.set_timeout(ms);
-
-	// Only retrieve IP datagrams which are sent from 192.168.0.1
-	snifferObject.set_filter("ip src 141.133.96.135");
-	// Retrieve the packet.
-	Packet packet = snifferObject.next_packet();
-	// Do something with some_pdu...
-	
+int sniffeth_internal(int ms, char * interface, int port, char * ipaddress)
+{	
 	int time = -1;
 
-	//try to find the IP packet internally for this PDU
-	if(packet.pdu()->find_pdu<IP>())
-	{
-		time = packet.timestamp().seconds();	
-	}
+	SnifferConfiguration config;
+	//add the port to the filter
+	char portStr[10];
+	snprintf(portStr,10,"port %d",port);
+	config.set_filter(portStr);
 
-	return time;
+	//set the config for the sniffer to be promiscous
+	config.set_promisc_mode(true);
+
+	//set the snap length
+	config.set_snap_len(400);
+
+	//set the ip address of the filter
+	char ipStr[100];
+	snprintf(ipStr,10,"ip src %s",ipaddress);
+	config.set_filter(ipStr);
+
+	//now make the sniffer object
+	Sniffer snifferObject(interface,config);
+
+	//set the timeout for the object
+	snifferObject.set_timeout(ms);
+
+	// Retrieve the next packet.
+	Packet packet = snifferObject.next_packet();
+	
+	currentPacketID++;
+
+	hash_table[currentPacketID] = &packet;
+
+	// //try to find the IP packet internally for this PDU
+	// if(packet.pdu()->find_pdu<IP>())
+	// {
+	// 	time = packet.timestamp().seconds();	
+	// }
+
+	return currentPacketID;
 }
 
 
-EXTERN_C DLLEXPORT int GetPacketTimestamp(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
+EXTERN_C DLLEXPORT int GetFullPacketMetadata(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
 {
    
 	//the first argument is the milliseconds timeout to use
 	int millisecond = (int) MArgument_getInteger(Args[0]);
 
+	//the second argument is the interface to sniff on
+	char * interface = MArgument_getUTF8String(Args[1]);
+
+	//the third argument is the port to listen on
+	int port = (int) MArgument_getInteger(Args[2]);
+
+	//the fourth argument is the ip address to listen on for the source
+	char * address = MArgument_getUTF8String(Args[3]);
+
+
 	//get the time value from the function
-	int timeRes = sniffeth_internal(millisecond);
+	int timeRes = sniffeth_internal(millisecond,interface,port,address);
+
+	//create an mtensor to return
+	MTensor returnTensor;
+	mint dims[] = {2};
+	int error = libData->MTensor_new(MType_MTensor,1,2);
 
 	//set that as the result
 	MArgument_setInteger(Result,timeRes);
@@ -68,4 +103,13 @@ EXTERN_C DLLEXPORT int GetPacketTimestamp(WolframLibraryData libData, mint Argc,
 	return LIBRARY_NO_ERROR;
 }
 
+
+int GetPacketProtocolName(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
+{
+	int packet_id = (int) MArgument_getInteger(Args[0]);
+
+	Packet * packet = hash_table[packet_id];
+
+	packet->getProtocolName();
+}
 
