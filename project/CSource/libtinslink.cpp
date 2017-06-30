65db4cd628e8129bfbc6d36eef88d24843bab0bd
Ian Johnson <ijohnson@wolfram.com>
2017-06-29 21:25:11 -0400

    Adding new function for asynchronous threading of the sniff_loop function

--- project/CSource/libtinslink.cpp
+++ project/CSource/libtinslink.cpp
@@ -6,29 +6,49 @@
 #include "WolframLibrary.h"
 
 #include <map>
+#include <atomic>
+#include <future>
 
-
-EXTERN_C DLLEXPORT mint WolframLibrary_getVersion(){
-  return WolframLibraryVersion;
-}
-
-EXTERN_C DLLEXPORT int WolframLibrary_initialize( WolframLibraryData libData) \
-{
-	return 0;
-}
-
-EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData \
-libData) {
-	return;
-}
 
 using namespace Tins;
 
-
+//map for individual packets
 std::map<int,Packet * > hash_table;
 
-
+//packetID for individual packet reading
 static int currentPacketID = 0;
+
+
+
+//for the continuous packet reading
+std::atomic<int> nextPacketID(0);
+
+//for determining when to stop the thread that copies packets
+std::atomic<bool> keepRunning(true);
+
+//map of all the packets gathered inside the background thread
+std::map<int,PDU *> continuousPacketTable = std::map<int,PDU *>();
+
+
+
+
+EXTERN_C DLLEXPORT mint WolframLibrary_getVersion()
+{
+  return WolframLibraryVersion;
+}
+
+EXTERN_C DLLEXPORT int WolframLibrary_initialize( WolframLibraryData libData)
+{
+	nextPacketID = 0;
+
+	return 0;
+}
+
+EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData libData)
+{
+	return;
+}
+
 
 int sniffeth_internal(int ms, char * interface, int port, char * ipaddress)
 {	
@@ -104,33 +124,105 @@ EXTERN_C DLLEXPORT int GetFullPacketMetadata(WolframLibraryData libData, mint Ar
 	return LIBRARY_NO_ERROR;
 }
 
-bool test(const PDU &pdu) {
-    const IP &ip = pdu.rfind_pdu<IP>();
-    const TCP &tcp = pdu.rfind_pdu<TCP>();
-
-    // the below four calls give out what we want. they get called in a loop when test() gets called again and again
-    ip.src_addr();
-    tcp.sport();
-
-    tcp.dport();
-    ip.dst_addr();
-
-    return true;
-}
- 
-
-EXTERN_C DLLEXPORT int testfunc(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
+bool tcpSniff(const PDU &pdu) 
 {
-	Sniffer sniffer("Wireless Network Connection");
-	sniffer.sniff_loop(test);
+	//make a clone of the pdu and store it into the hash table
+    continuousPacketTable[nextPacketID++] = pdu.clone();
+
+    return keepRunning;
+}
 
 
-	// pass the output of the above to mathematica
+void sniff_thread(std::string interface, int port, std::string ipaddress)
+{
+	SnifferConfiguration config;
+	//add the port to the filter
+	char portStr[10];
+	snprintf(portStr,10,"port %d",port);
+	config.set_filter(portStr);
 
+	//set the config for the sniffer to be promiscous
+	config.set_promisc_mode(true);
+
+	//set the snap length
+	config.set_snap_len(400);
+
+	//set the ip address of the filter
+	char ipStr[100];
+	snprintf(ipStr,10,"ip src %s",ipaddress.c_str());
+	config.set_filter(ipStr);
+
+	//now make the sniffer object
+	Sniffer snifferObject(interface,config);
+	snifferObject.sniff_loop(tcpSniff);
+}
+
+
+EXTERN_C DLLEXPORT int StartTCPSniffing(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
+{
+	//the first argument is the interface to sniff on
+	std::string interface(MArgument_getUTF8String(Args[0]));
+
+	//the second argument is the port to sniff on
+	int port = (int) MArgument_getInteger(Args[1]);
+
+	//the third argument is the source ip address to sniff for
+	std::string ipaddress(MArgument_getUTF8String(Args[2]));	
+
+	//mark the thread as start running
+	keepRunning = true;
+
+	//start the sniffer in a background thread
+	std::thread t(sniff_thread,interface,port,ipaddress);
 
 	return 0;
 
 }
+
+EXTERN_C DLLEXPORT int StopTCPSniffing(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
+{
+	//just mark the thread to stop running
+	keepRunning = false;	
+
+	return LIBRARY_NO_ERROR;
+
+}
+
+EXTERN_C DLLEXPORT int EmptyTCPSniffingHashTable(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
+{
+	// const IP &ip = pdu.rfind_pdu<IP>();
+    // const TCP &tcp = pdu.rfind_pdu<TCP>();
+
+	// the below four calls give out what we want. they get called in a loop when test() gets called again and again
+    // ip.src_addr();
+    // tcp.sport();
+
+    // tcp.dport();
+    // ip.dst_addr();
+
+    return LIBRARY_NO_ERROR;
+}
+
+EXTERN_C DLLEXPORT int TCPSniffingHashTableSize(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
+{
+	// const IP &ip = pdu.rfind_pdu<IP>();
+    // const TCP &tcp = pdu.rfind_pdu<TCP>();
+
+	// the below four calls give out what we want. they get called in a loop when test() gets called again and again
+    // ip.src_addr();
+    // tcp.sport();
+
+    // tcp.dport();
+    // ip.dst_addr();
+
+    MArgument_setInteger(Result,continuousPacketTable.size());
+
+    return LIBRARY_NO_ERROR;
+}
+  
+
+
+
 EXTERN_C DLLEXPORT int GetPacketProtocolName(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
 {
 	int packet_id = (int) MArgument_getInteger(Args[0]);
@@ -166,3 +258,4 @@ int dnssniff(int argc, char* argv[]) {
     sniffer.sniff_loop(dns);
     return true;
 }
+
