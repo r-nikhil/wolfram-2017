8f12d7df94b827219b0fe660bd4ada332f5938c0
Nikhil R <rnikhil96@outlook.com>
2017-07-04 21:52:31 -0400

    updated c++ code

--- project/CSource/libtinslink.cpp
+++ project/CSource/libtinslink.cpp
@@ -37,6 +37,7 @@ std::map<int,std::pair<Timestamp,PDU *> > continuousPacketTableUDP = std::map<in
 
 std::map<int,PDU *> continuousPacketTableDNS = std::map<int,PDU *>();
 
+std::thread t;
 
 EXTERN_C DLLEXPORT mint WolframLibrary_getVersion()
 {
@@ -56,81 +57,6 @@ EXTERN_C DLLEXPORT void WolframLibrary_uninitialize( WolframLibraryData libData)
 }
 
 
-int sniffeth_internal(int ms, char * interface, int port, char * ipaddress)
-{	
-	// int time = -1;
-
-	SnifferConfiguration config;
-	//add the port to the filter
-	char portStr[10];
-	snprintf(portStr,10,"port %d",port);
-	config.set_filter(portStr);
-
-	//set the config for the sniffer to be promiscous
-	config.set_promisc_mode(true);
-
-	//set the snap length
-	config.set_snap_len(400);
-
-	//set the ip address of the filter
-	char ipStr[100];
-	snprintf(ipStr,10,"ip src %s",ipaddress);
-	config.set_filter(ipStr);
-
-	//now make the sniffer object
-	Sniffer snifferObject(interface,config);
-
-	//set the timeout for the object
-	snifferObject.set_timeout(ms);
-
-	// Retrieve the next packet.
-	Packet packet = snifferObject.next_packet();
-	
-	currentPacketIDIndividual++;
-
-	hash_table[currentPacketIDIndividual] = &packet;
-
-	// //try to find the IP packet internally for this PDU
-	// if(packet.pdu()->find_pdu<IP>())
-	// {
-	// 	time = packet.timestamp().seconds();	
-	// }
-
-	return currentPacketIDIndividual;
-}
-
-
-EXTERN_C DLLEXPORT int GetFullPacketMetadata(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
-{
-   
-	//the first argument is the milliseconds timeout to use
-	int millisecond = (int) MArgument_getInteger(Args[0]);
-
-	//the second argument is the interface to sniff on
-	char * interface = MArgument_getUTF8String(Args[1]);
-
-	//the third argument is the port to listen on
-	int port = (int) MArgument_getInteger(Args[2]);
-
-	//the fourth argument is the ip address to listen on for the source
-	char * address = MArgument_getUTF8String(Args[3]);
-
-
-	//get the time value from the function
-	int timeRes = sniffeth_internal(millisecond,interface,port,address);
-
-	//create an mtensor to return
-	// MTensor returnTensor;
-	// mint dims[] = {2};
-	// int error = libData->MTensor_new(MType_Integer,1,dims,&returnTensor);
-
-	//set that as the result
-	MArgument_setInteger(Result,timeRes);
-
-	return LIBRARY_NO_ERROR;
-}
-
-std::thread t;
 
 EXTERN_C DLLEXPORT int listDefaultInterface(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
 {
@@ -192,7 +118,6 @@ EXTERN_C DLLEXPORT int listAllInterfaces(WolframLibraryData libData, mint Argc, 
 
 	return LIBRARY_NO_ERROR;
 }
-
 
 bool dnsSniff(const PDU &pdu) 
 {
@@ -496,15 +421,6 @@ EXTERN_C DLLEXPORT int EmptyTCPSniffingHashTable(WolframLibraryData libData, min
     return LIBRARY_NO_ERROR;
 }
 
-// EXTERN_C DLLEXPORT int GetPacketProtocolName(WolframLibraryData libData, mint Argc, MArgument *Args, MArgument Result)
-// {
-// 	int packet_id = (int) MArgument_getInteger(Args[0]);
-
-// 	Packet * packet = hash_table[packet_id];
-// 	return 0;
-// 	// packet->getProtocolName();
-// }
-
 
 bool udpSniff(Packet & pkt) 
 {
@@ -590,7 +506,9 @@ EXTERN_C DLLEXPORT int EmptyUDPSniffingHashTable(WolframLibraryData libData, min
 
 					std::stringstream ss;
 
-					ss << ip.src_addr() << ":" << udp.sport() << "to" << ip.dst_addr() << ":" << udp.dport() << "checksum" << udp.checksum() << "length" << udp.length() << "headersize" << udp.header_size();
+					ss << ip.src_addr() << ":" << udp.sport() << "to" << ip.dst_addr() << ":" << udp.dport() << "checksum" << udp.checksum() << "length" << udp.length() << "headersize" << udp.header_size()<<
+						"ts" << continuousPacketTableTCP[x].first.seconds() << 
+						"tus" << continuousPacketTableTCP[x].first.microseconds();;
 
 					std::string s = ss.str();
 
@@ -639,7 +557,9 @@ EXTERN_C DLLEXPORT int EmptyUDPSniffingHashTable(WolframLibraryData libData, min
 
 					std::stringstream ss;
 
-					ss << ip.src_addr() << ":" << udp.sport() << "to" << ip.dst_addr() << ":" << udp.dport() << "checksum" << udp.checksum() << "length" << udp.length() << "headersize" << udp.header_size();
+					ss << ip.src_addr() << ":" << udp.sport() << "to" << ip.dst_addr() << ":" << udp.dport() << "checksum" << udp.checksum() << "length" << udp.length() << "headersize" << udp.header_size()<<
+						"ts" << continuousPacketTableTCP[x].first.seconds() << 
+						"tus" << continuousPacketTableTCP[x].first.microseconds();;
 
 					std::string s = ss.str();
 
